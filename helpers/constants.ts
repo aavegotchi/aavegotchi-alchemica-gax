88aca8d93d335433e48e4f492573f8e3326fb0dd
Coderdan <dmathieuva@gmail.com>
2022-08-31 17:03:38 +0800

    coreprop prep

--- helpers/constants.ts
+++ helpers/constants.ts
@@ -1,13 +1,12 @@
 import { BigNumber } from "ethers";
-import {
-  AlchemicaParams,
-} from "./types";
+import { AlchemicaParams } from "./types";
 
 export const GWEI = BigNumber.from(1e9);
 export const ETHER = GWEI.mul(GWEI);
 export const YEAR = 60 * 60 * 24 * 365;
 
-export const DAO_TREASURY_ADDRESS = "0x6fb7e0AAFBa16396Ad6c1046027717bcA25F821f";
+export const DAO_TREASURY_ADDRESS =
+  "0x6fb7e0AAFBa16396Ad6c1046027717bcA25F821f";
 export const WMATIC_ADDRESS = "0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270";
 export const ECOSYSTEM_VESTING_BENEFICIARY = DAO_TREASURY_ADDRESS;
 export const GAMEPLAY_VESTING_BENEFICIARY = DAO_TREASURY_ADDRESS;
@@ -40,3 +39,5 @@ export const KEK_PARAMS: AlchemicaParams = {
   symbol: "KEK",
   supply: BigNumber.from(10).mul(ETHER).mul(GWEI),
 };
+
+export const farmAddress = "0x1fE64677Ab1397e20A1211AFae2758570fEa1B8c";
---
+++ scripts/updateFarms.ts
@@ -0,0 +1,41 @@
+import { ethers, network } from "hardhat";
+import { FarmFacet } from "../typechain-types/FarmFacet";
+import { impersonate } from "../scripts/helperFunctions";
+import { farmAddress } from "../helpers/constants";
+
+async function main() {
+  const owner = "0x94cb5C277FCC64C274Bd30847f0821077B231022";
+
+  let farmFacet = (await ethers.getContractAt(
+    "FarmFacet",
+    farmAddress
+  )) as FarmFacet;
+
+  if (network.name === "localhost" || network.name === "hardhat") {
+    farmFacet = await impersonate(owner, farmFacet, ethers, network);
+  }
+
+  //   console.log("Remove GHST-WMATIC rewards");
+  //   let tx = await farmFacet.set(
+  //     "6", //ghst-wmatic
+  //     "0",
+  //     true
+  //   );
+  //   await tx.wait();
+
+  //   console.log("Set GHST-USDC to 20%");
+  //   tx = await farmFacet.set("5", "4", true);
+  //   await tx.wait();
+
+  for (let i = 0; i < 10; i++) {
+    const poolInfo = await farmFacet.poolInfo(i);
+    console.log("Pool info:", poolInfo);
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
